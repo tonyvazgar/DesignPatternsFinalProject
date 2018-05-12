e21593b70e551d91747710b31d459d6a8b48a684
tonyvazgar <luis.vazquezga@udlap.mx>
2018-05-12 16:31:14 -0500

    Final Commit

--- src/Intro.java
+++ src/Intro.java
@@ -7,8 +7,8 @@ public class Intro extends Frame{
         JOptionPane.showMessageDialog(this,
                 "LUIS ANTONIO VAZQUEZ GARCIA                                                                                            ID: 153675 \n\n" +
                         "Mismo proyecto que el de inteligencia artificial con estos patrones de diseño:\n\n" +
-                        "--> MEMENTO: Para poder regresar a un estado anterior (una pregunta anterior).\n" +
-                        "--> SINGLETON: Para poder tener una instancia del memento y asi validar respuestas anterores.\n" +
+                        "--> MEMENTO: Para poder regresar a un estado anterior (una pregunta anterior) guardando el estado de la pregunta.\n" +
+                        "--> SINGLETON: Para poder tener una solo un objeto del memento y asi validar respuestas anterores. Ya que se accede a memento en varias clases.\n" +
                         "--> TEMPLATE METHOD: Para organizar las diferentes partes del algoritmo y sea más fácil interpretarlo.",
                 "Información",
                 JOptionPane.INFORMATION_MESSAGE);
